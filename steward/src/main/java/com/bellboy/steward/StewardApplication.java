431eda60980cc350989121a0486569c992989f47
Your Actual Name <atharvaguldagad@gmail.com>
2026-09-21 22:24:15 +0530

    added a restBean

--- steward/src/main/java/com/bellboy/steward/StewardApplication.java
+++ steward/src/main/java/com/bellboy/steward/StewardApplication.java
@@ -3,11 +3,19 @@ package com.bellboy.steward;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 @SpringBootApplication
 public class StewardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StewardApplication.class, args);
 	}
-
+	@Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
